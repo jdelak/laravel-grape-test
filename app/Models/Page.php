6cec9c448b63f7d3b37b44057f540c00f4e63b54
jdelak <delakdev@gmail.com>
2022-10-25 23:38:27 +0200

    jetstream livewire + adding grapejs

---
+++ app/Models/Page.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Dotlogics\Grapesjs\App\Traits\EditableTrait;
+use Dotlogics\Grapesjs\App\Contracts\Editable;
+
+class Page extends Model implements Editable
+{
+    use HasFactory;
+    use EditableTrait;
+
+    protected $fillable = [
+        'title',
+        'gjs_data',
+    ];
+}
